964dcdd77ec9e411b738e8463f3a0569ea784946
Yongwoo Lee <whylee@icon.foundation>
2018-07-27 15:53:05 +0900

    Update junit version(junit5)

--- library/src/test/java/foundation/icon/icx/IconServiceTest.java
+++ library/src/test/java/foundation/icon/icx/IconServiceTest.java
@@ -1,10 +1,12 @@
 package foundation.icon.icx;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-public class IconServiceTest {
-    @Test public void testIconServiceInit() {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class IconServiceTest {
+    @Test
+    void testIconServiceInit() {
         IconService iconService = new IconService(new Provider() {
             @Override
             public <T> Call<T> request(Request request) {
