ff26dc75341c910bccd193ed11dc70afb8dec6a7
amuyu <hwajoong.song@gmail.com>
2018-08-03 16:12:53 +0900

    Add sample codes.

---
+++ samples/src/main/java/com/example/iconsdk/ScoreQuery.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 theloop Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.iconsdk;
+
+import foundation.icon.icx.IconService;
+import foundation.icon.icx.IcxCall;
+import foundation.icon.icx.transport.http.HttpProvider;
+import foundation.icon.icx.transport.jsonrpc.RpcField;
+import foundation.icon.icx.transport.jsonrpc.RpcObject;
+import foundation.icon.icx.transport.jsonrpc.RpcValue;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import java.io.IOException;
+
+public class ScoreQuery {
+
+    public final String URL = "http://localhost:9000/api/v3";
+    public final String PRIVATE_KEY_STRING =
+            "2d42994b2f7735bbc93a3e64381864d06747e574aa94655c516f9ad0a74eed79";
+    private final String scoreAddress = "cx2e6032c7598b882da4b156ed9334108a5b87f2dc";
+
+    private IconService iconService;
+
+    public ScoreQuery() {
+        HttpLoggingInterceptor loggning = new HttpLoggingInterceptor();
+        loggning.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggning)
+                .build();
+        iconService = new IconService(new HttpProvider(httpClient, URL));
+    }
+
+    public void query() throws IOException {
+        String fromAddress = "hx4873b94352c8c1f3b2f09aaeccea31ce9e90bd31";
+
+        RpcObject params = new RpcObject.Builder()
+                .put("_owner", new RpcValue(fromAddress))
+                .build();
+
+        IcxCall<RpcField> call = new IcxCall.Builder()
+                .from(fromAddress)
+                .to(scoreAddress)
+                .method("balanceOf")
+                .params(params)
+                .build();
+
+        RpcField result = iconService.query(call).execute();
+        System.out.println("result:"+result.asInteger());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ScoreQuery().query();
+    }
+
+}
---
+++ samples/src/main/java/com/example/iconsdk/SendTokenTransaction.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 theloop Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.iconsdk;
+
+import foundation.icon.icx.*;
+import foundation.icon.icx.transport.http.HttpProvider;
+import foundation.icon.icx.transport.jsonrpc.RpcObject;
+import foundation.icon.icx.transport.jsonrpc.RpcValue;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.web3j.utils.Convert;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class SendTokenTransaction {
+
+    public final String URL = "http://localhost:9000/api/v3";
+    public final String PRIVATE_KEY_STRING =
+            "2d42994b2f7735bbc93a3e64381864d06747e574aa94655c516f9ad0a74eed79";
+    private final String scoreAddress = "cx2e6032c7598b882da4b156ed9334108a5b87f2dc";
+
+    private IconService iconService;
+    private Wallet wallet;
+
+    public SendTokenTransaction() {
+        HttpLoggingInterceptor loggning = new HttpLoggingInterceptor();
+        loggning.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggning)
+                .build();
+        iconService = new IconService(new HttpProvider(httpClient, URL));
+        wallet = KeyWallet.load(PRIVATE_KEY_STRING);
+    }
+
+    public void sendTransaction() throws IOException {
+        BigInteger networkId = new BigInteger("3");
+        String fromAddress = wallet.getAddress();
+        String toAddress = "hx4873b94352c8c1f3b2f09aaeccea31ce9e90bd31";
+        BigInteger value = Convert.toWei("1", Convert.Unit.ICX).toBigInteger();
+        BigInteger stepLimit = new BigInteger("75000");
+        long timestamp = System.currentTimeMillis() * 1000L;
+        BigInteger nonce = new BigInteger("1");
+        String methodName = "transfer";
+
+
+        RpcObject params = new RpcObject.Builder()
+                .put("_to", new RpcValue(toAddress))
+                .put("_value", new RpcValue(value))
+                .build();
+
+        Transaction transaction = TransactionBuilder.of(networkId)
+                .from(fromAddress)
+                .to(scoreAddress)
+                .stepLimit(stepLimit)
+                .timestamp(new BigInteger(Long.toString(timestamp)))
+                .nonce(nonce)
+                .call(methodName)
+                .params(params)
+                .build();
+
+        SignedTransaction signedTransaction = new SignedTransaction(transaction, wallet);
+        String hash = iconService.sendTransaction(signedTransaction).execute();
+        System.out.println("txHash:"+hash);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new SendTokenTransaction().sendTransaction();
+    }
+
+}
