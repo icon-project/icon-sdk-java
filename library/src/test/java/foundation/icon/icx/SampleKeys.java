72582018a3a1bc599f9fefb21485cd739233f439
amuyu <hwajoong.song@gmail.com>
2018-08-03 11:45:05 +0900

    Extract as sample key class to use in test.

---
+++ library/src/test/java/foundation/icon/icx/SampleKeys.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018 theloop Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package foundation.icon.icx;
+
+public class SampleKeys {
+
+    public static final String PRIVATE_KEY_STRING =
+            "2d42994b2f7735bbc93a3e64381864d06747e574aa94655c516f9ad0a74eed79";
+
+    public static final String ADDRESS = "hx4873b94352c8c1f3b2f09aaeccea31ce9e90bd31";
+    public static final String PASSWORD = "Pa55w0rd";
+}
